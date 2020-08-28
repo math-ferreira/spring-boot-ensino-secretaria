4753b098131ed855211f48981f0d09e4baab6fad
reidomath <mat.s.ferreira@gmail.com>
2020-08-28 19:42:33 -0300

    primeiro commit

---
+++ src/test/java/com/springboot/boaspraticas/apisecretaria/ApiSecretariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.boaspraticas.apisecretaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSecretariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
