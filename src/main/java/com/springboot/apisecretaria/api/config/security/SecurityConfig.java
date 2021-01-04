127352e4ddbeb4865c87674d63dd7c46e565089f
reidomath <mat.s.ferreira@gmail.com>
2021-01-03 23:13:19 -0300

    liberando acesso as rotas do Swagger UI

--- src/main/java/com/springboot/apisecretaria/api/config/security/SecurityConfig.java
+++ src/main/java/com/springboot/apisecretaria/api/config/security/SecurityConfig.java
@@ -22,56 +22,60 @@ import com.springboot.apisecretaria.service.UsuarioService;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UsuarioService autenticacaoService;
-	
-	@Autowired
-	private TokenService tokenService;
-	
-	@Autowired
-	private UsuarioService usuarioService;
-	
-	/*
-	 * Recebe uma auth, metodo que serve para config a parte de autenticação,
-	 * controle de acesso, login e etc
-	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-	}
+    @Autowired
+    private UsuarioService autenticacaoService;
 
-	// Configuração de autorizações, como URL, quem acesso, perfil de acesso e etc - Irmeos utilizar em nosso projeto
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/alunos").permitAll()
-		.antMatchers(HttpMethod.GET, "/alunos/*").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoTokenFilter(tokenService, usuarioService), UsernamePasswordAuthenticationFilter.class);
-	}
+    @Autowired
+    private TokenService tokenService;
 
-	/*
-	 * Configurações de recursos estaticos, como requisição de arquivos, js, css //
-	 * imagens e etc - no nosso caso de backend não afz sentido, mas sim se fosse
-	 * MVC
-	 */
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		
-	}
-	
-	@Override
-	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-	
-	public static void main(String[] args) {
-		BCryptPasswordEncoder cript = new BCryptPasswordEncoder();
-		System.out.println(cript.encode("senha123"));
-	}
+    @Autowired
+    private UsuarioService usuarioService;
+
+    /*
+     * Recebe uma auth, metodo que serve para config a parte de autenticação,
+     * controle de acesso, login e etc
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    // Configuração de autorizações, como URL, quem acesso, perfil de acesso e etc - Irmeos utilizar em nosso projeto
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/alunos").permitAll()
+                .antMatchers(HttpMethod.GET, "/alunos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
+                .antMatchers(HttpMethod.GET, "/v2/api-docs/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger.json").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger-resources/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoTokenFilter(tokenService, usuarioService), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    /*
+     * Configurações de recursos estaticos, como requisição de arquivos, js, css //
+     * imagens e etc - no nosso caso de backend não afz sentido, mas sim se fosse
+     * MVC
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder cript = new BCryptPasswordEncoder();
+        System.out.println(cript.encode("senha123"));
+    }
 }
